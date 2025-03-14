8fb21e35a345299100dee3b8c7aa360de22e8c71
Marta Tavera <martatavera@gmail.com>
2025-03-14 14:06:43 +0000

    random questions generator and background image added

--- TimesTables/src/App.tsx
+++ TimesTables/src/App.tsx
@@ -1,46 +1,49 @@
-import { SetStateAction, useState, useRef } from 'react';
-
+import { SetStateAction, useState, useRef, useEffect } from 'react';
+import MathsCover from './assets/MathsCover.png'
 import './App.css';
 
 
 function App() {
+  function generateMultiplicationQuestion():string{
+    const factor1 = Math.floor(Math.random() * 12  + 1);
+    const factor2 =  Math.floor(Math.random() * 12 + 1);
+    const correctAnswer = factor1 * factor2;
+    const question = `${factor1} × ${factor2} `;
+    return question 
+  }
 
-
-  
-  const questions = [
-    "2 x 4", "3 x 9", "9 x 5", "6 x 7", "8 x 4"   
-  ];
-  
+  const [questions, setQuestions] = useState<string[]>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userAnswer, setUserAnswer] = useState("");
   const inputRef = useRef(null);
-
-  const nextQuestion = () => {
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      setCurrentQuestionIndex(questions.length); // Set to length to show final score
+  
+useEffect(()=>{
+  const generateQuestions = (count = 10)=>{
+    const newQuestions: string[] = [];
+    for (let i = 0; i < count; i++) {
+      newQuestions.push(generateMultiplicationQuestion());
+      console.log("pq no veo mis preguntas loop", i);
     }
+    console.log("Questions:", newQuestions);
+    return newQuestions;
   };
+    setQuestions(generateQuestions());
+}, []);
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    
-    const currentAnswer = userAnswer; 
-    // Clear the input immediately
-    setUserAnswer("");
-    
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      setCurrentQuestionIndex(questions.length);
-    }
- 
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  };
 
+const handleSubmit = (e: { preventDefault: () => void; }) => {
+  e.preventDefault();
+  const currentAnswer = userAnswer; 
+  setUserAnswer("");
+  if (currentQuestionIndex < questions.length - 1) {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  } else {
+    setCurrentQuestionIndex(questions.length);
+  }
+  if (inputRef.current) {
+    inputRef.current.focus();
+    }
+};
 const handleInputChange = (e: { target: { value: SetStateAction<string>; }; }) => {
   setUserAnswer(e.target.value);
 };
@@ -63,7 +66,7 @@ return (
         </form>
       </div>   
       ) : (
-        <div>Quiz completed!</div>
+        <div className="end-quiz-text">Quiz completed!</div>
       )}
     </div>  
   
