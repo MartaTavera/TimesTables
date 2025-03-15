863e78a6944da7afa06b5d275beb1792cf3d365e
Marta Tavera <martatavera@gmail.com>
2025-03-15 12:02:06 +0000

    added feedback for each question

--- TimesTables/src/App.tsx
+++ TimesTables/src/App.tsx
@@ -4,44 +4,62 @@ import './App.css';
 
 
 function App() {
-  function generateMultiplicationQuestion():string{
-    const factor1 = Math.floor(Math.random() * 12  + 1);
-    const factor2 =  Math.floor(Math.random() * 12 + 1);
+  function generateMultiplicationQuestion():{ question: string, correctAnswer: number }{
+    const factor1 = Math.floor(Math.random() * 10  + 2);
+    const factor2 =  Math.floor(Math.random() * 10 + 2);
     const correctAnswer = factor1 * factor2;
     const question = `${factor1} × ${factor2} `;
-    return question 
+    console.log(factor1, factor2)
+    console.log("answers ", correctAnswer);
+    return {question, correctAnswer}
   }
-
   const [questions, setQuestions] = useState<string[]>([]);
+  const [correctAnswers, setCorrectAnswers] = useState<number[]>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [correctAnswersIndex, setCorrectAnswersIndex] = useState(0)
+  const [feedback, setFeedback] = useState(false);
   const [userAnswer, setUserAnswer] = useState("");
+  const [result, setResult] = useState("");
   const inputRef = useRef(null);
   
 useEffect(()=>{
-  const generateQuestions = (count = 10)=>{
-    const newQuestions: string[] = [];
+  const generateQuestions = (count = 3)=>{
+    const newQuestions =[];
+    const correctAnswers =[];
     for (let i = 0; i < count; i++) {
-      newQuestions.push(generateMultiplicationQuestion());
+      const generatedQuestion = generateMultiplicationQuestion();
+      newQuestions.push(generatedQuestion.question);
+      correctAnswers.push(generatedQuestion.correctAnswer);
     }
-    return newQuestions;
+    setQuestions(newQuestions);
+    setCorrectAnswers(correctAnswers);
+
+    console.log("Questions:", newQuestions);
+    console.log("Answers: ",correctAnswers);
   };
-    setQuestions(generateQuestions());
+ generateQuestions();
 }, []);
 
 
 const handleSubmit = (e: { preventDefault: () => void; }) => {
   e.preventDefault();
-  const currentAnswer = userAnswer; 
+  const correctResult = (parseInt(userAnswer) === correctAnswers[currentQuestionIndex]);
+    setResult(correctResult? "correct": "incorrect")
+    
+  setFeedback(true);
   setUserAnswer("");
   if (currentQuestionIndex < questions.length - 1) {
     setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setCorrectAnswersIndex(currentQuestionIndex + 1);
   } else {
     setCurrentQuestionIndex(questions.length);
+    setCorrectAnswersIndex(questions.length)
   }
   if (inputRef.current) {
     inputRef.current.focus();
     }
 };
+
 const handleInputChange = (e: { target: { value: SetStateAction<string>; }; }) => {
   setUserAnswer(e.target.value);
 };
@@ -62,7 +80,13 @@ return (
           autoFocus
           />
         </form>
-      </div>   
+        {feedback && (
+          <div className="answer-feedback">
+          {result == "correct" ? '✓' : `✗`}
+        </div>  
+         )}
+      
+      </div>
       ) : (
         <div className="end-quiz-text">Quiz completed!</div>
       )}
